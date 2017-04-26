fde4bf9ef6ce351b0b24345a6eb477eb46838de8
Sudok <glauber.gw@gmail.com>
2017-04-26 13:08:48 -0300

    Uploader concluido e configurado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419211035) do
+ActiveRecord::Schema.define(version: 20170426155345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170419211035) do
     t.text     "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
+    t.json     "documents"
   end
 
 end
